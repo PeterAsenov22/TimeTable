d29ba76cee05e9a5f4c67d290eb61ab33b1fc99b
Peter Asenov <peter12345@abv.bg>
2020-03-19 17:46:18 +0200

    add MainForm

---
+++ TimeTable.UI/MainForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace TimeTable.UI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.employeesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerEmployeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchEmployeesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchComboBox = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.projectId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.startDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.endDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maxWH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.employeesToolStripMenuItem,
+            this.projectsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // employeesToolStripMenuItem
+            // 
+            this.employeesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registerEmployeeToolStripMenuItem,
+            this.searchEmployeesToolStripMenuItem});
+            this.employeesToolStripMenuItem.Name = "employeesToolStripMenuItem";
+            this.employeesToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.employeesToolStripMenuItem.Text = "Employees";
+            // 
+            // registerEmployeeToolStripMenuItem
+            // 
+            this.registerEmployeeToolStripMenuItem.Name = "registerEmployeeToolStripMenuItem";
+            this.registerEmployeeToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.registerEmployeeToolStripMenuItem.Text = "Register Employee";
+            // 
+            // searchEmployeesToolStripMenuItem
+            // 
+            this.searchEmployeesToolStripMenuItem.Name = "searchEmployeesToolStripMenuItem";
+            this.searchEmployeesToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.searchEmployeesToolStripMenuItem.Text = "Search Employees";
+            // 
+            // projectsToolStripMenuItem
+            // 
+            this.projectsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registerProjectToolStripMenuItem});
+            this.projectsToolStripMenuItem.Name = "projectsToolStripMenuItem";
+            this.projectsToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.projectsToolStripMenuItem.Text = "Projects";
+            // 
+            // registerProjectToolStripMenuItem
+            // 
+            this.registerProjectToolStripMenuItem.Name = "registerProjectToolStripMenuItem";
+            this.registerProjectToolStripMenuItem.Size = new System.Drawing.Size(196, 26);
+            this.registerProjectToolStripMenuItem.Text = "Register Project";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.searchButton);
+            this.groupBox1.Controls.Add(this.searchTextBox);
+            this.groupBox1.Controls.Add(this.searchComboBox);
+            this.groupBox1.Location = new System.Drawing.Point(46, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(706, 68);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Find Project";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Find By:";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(588, 19);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(85, 30);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(385, 23);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(168, 22);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // searchComboBox
+            // 
+            this.searchComboBox.FormattingEnabled = true;
+            this.searchComboBox.Location = new System.Drawing.Point(186, 23);
+            this.searchComboBox.Name = "searchComboBox";
+            this.searchComboBox.Size = new System.Drawing.Size(156, 24);
+            this.searchComboBox.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.projectId,
+            this.name,
+            this.description,
+            this.startDate,
+            this.endDate,
+            this.maxWH});
+            this.dataGridView1.Location = new System.Drawing.Point(46, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 300);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // projectId
+            // 
+            this.projectId.HeaderText = "Project Id";
+            this.projectId.MinimumWidth = 6;
+            this.projectId.Name = "projectId";
+            this.projectId.ReadOnly = true;
+            this.projectId.Width = 125;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 125;
+            // 
+            // description
+            // 
+            this.description.HeaderText = "Description";
+            this.description.MinimumWidth = 6;
+            this.description.Name = "description";
+            this.description.ReadOnly = true;
+            this.description.Width = 125;
+            // 
+            // startDate
+            // 
+            this.startDate.HeaderText = "Start Date";
+            this.startDate.MinimumWidth = 6;
+            this.startDate.Name = "startDate";
+            this.startDate.ReadOnly = true;
+            this.startDate.Width = 125;
+            // 
+            // endDate
+            // 
+            this.endDate.HeaderText = "End Date";
+            this.endDate.MinimumWidth = 6;
+            this.endDate.Name = "endDate";
+            this.endDate.ReadOnly = true;
+            this.endDate.Width = 125;
+            // 
+            // maxWH
+            // 
+            this.maxWH.HeaderText = "Max Working Hours";
+            this.maxWH.MinimumWidth = 6;
+            this.maxWH.Name = "maxWH";
+            this.maxWH.ReadOnly = true;
+            this.maxWH.Width = 125;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem employeesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registerEmployeeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchEmployeesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registerProjectToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ComboBox searchComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn projectId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn startDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn endDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maxWH;
+    }
+}
---
+++ TimeTable.UI/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTable.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] searchByItems = { "Name", "Start Date", "End Date", "Description", "Status" };
+            searchComboBox.DataSource = searchByItems;
+            searchComboBox.SelectedIndex = 0;
+
+            string[] row = {
+                "123456",
+                "Demo Project",
+                "Very cool project about ...",
+                "26.02.2019",
+                "23.05.2020",
+                "120"
+            };
+
+            DataGridViewButtonColumn moreButtonColumn = new DataGridViewButtonColumn();
+            moreButtonColumn.Name = "more";
+            moreButtonColumn.HeaderText = "";
+            moreButtonColumn.Text = "More";
+            moreButtonColumn.UseColumnTextForButtonValue = true;
+
+            dataGridView1.Columns.Add(moreButtonColumn);
+            dataGridView1.Rows.Add(row);
+        }
+    }
+}
