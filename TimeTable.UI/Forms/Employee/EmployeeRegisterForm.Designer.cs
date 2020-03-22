880b1426c57c46c0bb08087ff7b35c99e6df136b
TsvetanAtanasov <tzatanasov@gmail.com>
2020-03-22 19:51:26 +0200

    new employee form

---
+++ TimeTable.UI/Forms/Employee/EmployeeRegisterForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace TimeTable.UI.Forms.Employee
+{
+    partial class EmployeeRegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.endDateLabel = new System.Windows.Forms.Label();
+            this.startDateLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.maxWorkingHoursLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.registerProjectBtn = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.projectIdLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.projectIdTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // endDateLabel
+            // 
+            this.endDateLabel.AutoSize = true;
+            this.endDateLabel.Location = new System.Drawing.Point(450, 238);
+            this.endDateLabel.Name = "endDateLabel";
+            this.endDateLabel.Size = new System.Drawing.Size(67, 17);
+            this.endDateLabel.TabIndex = 31;
+            this.endDateLabel.Text = "End Date";
+            // 
+            // startDateLabel
+            // 
+            this.startDateLabel.AutoSize = true;
+            this.startDateLabel.Location = new System.Drawing.Point(135, 238);
+            this.startDateLabel.Name = "startDateLabel";
+            this.startDateLabel.Size = new System.Drawing.Size(72, 17);
+            this.startDateLabel.TabIndex = 30;
+            this.startDateLabel.Text = "Start Date";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(453, 262);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(213, 22);
+            this.dateTimePicker2.TabIndex = 29;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(138, 262);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(213, 22);
+            this.dateTimePicker1.TabIndex = 28;
+            // 
+            // maxWorkingHoursLabel
+            // 
+            this.maxWorkingHoursLabel.AutoSize = true;
+            this.maxWorkingHoursLabel.Location = new System.Drawing.Point(450, 159);
+            this.maxWorkingHoursLabel.Name = "maxWorkingHoursLabel";
+            this.maxWorkingHoursLabel.Size = new System.Drawing.Size(131, 17);
+            this.maxWorkingHoursLabel.TabIndex = 27;
+            this.maxWorkingHoursLabel.Text = "Max Working Hours";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(450, 84);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(79, 17);
+            this.descriptionLabel.TabIndex = 26;
+            this.descriptionLabel.Text = "Description";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(453, 185);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(213, 22);
+            this.textBox1.TabIndex = 25;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(453, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 22);
+            this.textBox2.TabIndex = 24;
+            // 
+            // registerProjectBtn
+            // 
+            this.registerProjectBtn.Location = new System.Drawing.Point(324, 334);
+            this.registerProjectBtn.Name = "registerProjectBtn";
+            this.registerProjectBtn.Size = new System.Drawing.Size(159, 33);
+            this.registerProjectBtn.TabIndex = 23;
+            this.registerProjectBtn.Text = "Register Project";
+            this.registerProjectBtn.UseVisualStyleBackColor = true;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(135, 159);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(45, 17);
+            this.nameLabel.TabIndex = 22;
+            this.nameLabel.Text = "Name";
+            // 
+            // projectIdLabel
+            // 
+            this.projectIdLabel.AutoSize = true;
+            this.projectIdLabel.Location = new System.Drawing.Point(135, 84);
+            this.projectIdLabel.Name = "projectIdLabel";
+            this.projectIdLabel.Size = new System.Drawing.Size(67, 17);
+            this.projectIdLabel.TabIndex = 21;
+            this.projectIdLabel.Text = "Project Id";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(138, 185);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(213, 22);
+            this.passwordTextBox.TabIndex = 20;
+            // 
+            // projectIdTextBox
+            // 
+            this.projectIdTextBox.Location = new System.Drawing.Point(138, 109);
+            this.projectIdTextBox.Name = "projectIdTextBox";
+            this.projectIdTextBox.Size = new System.Drawing.Size(213, 22);
+            this.projectIdTextBox.TabIndex = 19;
+            // 
+            // EmployeeRegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.endDateLabel);
+            this.Controls.Add(this.startDateLabel);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.maxWorkingHoursLabel);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.registerProjectBtn);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.projectIdLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.projectIdTextBox);
+            this.Name = "EmployeeRegisterForm";
+            this.Text = "EmployeeRegisterForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label endDateLabel;
+        private System.Windows.Forms.Label startDateLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label maxWorkingHoursLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button registerProjectBtn;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label projectIdLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox projectIdTextBox;
+    }
+}
